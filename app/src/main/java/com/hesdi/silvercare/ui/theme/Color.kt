68a540e6c19acab36f4bf6a89eb5c8e75966e7d7
Petterson04 <ferpetterson@gmail.com>
2025-03-26 08:41:57 -0600

    Create the login frame and vinculation firebase in the proyect

--- app/src/main/java/com/hesdi/silvercare/ui/theme/Color.kt
+++ app/src/main/java/com/hesdi/silvercare/ui/theme/Color.kt
@@ -2,6 +2,18 @@ package com.hesdi.silvercare.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+
+
+val verde = Color(0,92, 83)
+val amarillo= Color(159,193,49)
+val azulRey= Color(4,41,64)
+val azulCielo= Color(34,77,115)
+
+
+
+
+
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
