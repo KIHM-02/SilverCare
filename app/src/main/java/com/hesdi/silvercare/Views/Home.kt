43fa7044838c195e170a8fdab6b7e6ad29c7455c
Kevin Yael <kevinyael2010@gmail.com>
2025-04-04 21:26:15 -0600

    Se agrego los botones principales en la vista Home, aun falta su funcionamineto

--- app/src/main/java/com/hesdi/silvercare/Views/Home.kt
+++ app/src/main/java/com/hesdi/silvercare/Views/Home.kt
@@ -2,6 +2,7 @@ package com.hesdi.silvercare.Views
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,25 +10,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -62,50 +71,67 @@ fun HomeFrame() {
                     azulRey, azulCielo
                 )
             )
-        )
+        ),
+        contentAlignment = Alignment.Center
     ) {
-        OutlinedButton(
-            onClick = {
-            },
-            modifier = Modifier
-                .padding(15.dp)
-                .border(2.dp, Color.Transparent, RoundedCornerShape(12.dp))
-                .width(500.dp),
+       Column(
+           horizontalAlignment = Alignment.CenterHorizontally,
+           verticalArrangement = Arrangement.spacedBy(20.dp),
+           modifier = Modifier.padding(16.dp)
+       ){
+           SeccionPerfil()
+           MenuBotones("Recordatorios",R.drawable.applogo4) {/*Aqui se implementa la funcion del boton*/}
+           MenuBotones("Citas medicas",R.drawable.applogo4) {/*Aqui se implementa la funcion del boton*/}
+           MenuBotones("SOS",R.drawable.applogo4) {/*Aqui se implementa la funcion del boton*/}
+       }
+    }
+}
 
-            colors = ButtonDefaults.outlinedButtonColors(
-                containerColor = amarillo,
-                contentColor = MaterialTheme.colorScheme.onPrimary
-            ),
-            shape = RoundedCornerShape(12.dp),
+//Parte superior para visualizar el perfil, trabajando en ello 💪
+@Composable
+fun SeccionPerfil(){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ){
 
-            ) {
-            Text(
-                text = "Medicamentos",
-                fontWeight = FontWeight.Bold,
-                fontSize = 30.sp
+    }
+}
+
+//Funcion de botones, se supone que esto funciona correctamente
+@Composable
+fun MenuBotones(
+    text:String, iconRes: Int, onClick: () -> Unit
+){
+    OutlinedButton(
+        onClick = onClick,
+        colors = ButtonDefaults.outlinedButtonColors(
+            containerColor = amarillo,
+            contentColor = MaterialTheme.colorScheme.onPrimary
+        ),
+        shape = RoundedCornerShape(12.dp),
+        modifier = Modifier.fillMaxWidth(0.7f).height(50.dp)
+    ) {
+        Row (
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Icon(
+                painter = painterResource(id = iconRes),
+                contentDescription = text,
+                tint = Color.White,
+                modifier = Modifier.size(20.dp)
             )
-
-        }
-        OutlinedButton(
-            onClick = {},
-            modifier = Modifier
-                .padding(15.dp)
-                .border(2.dp, Color.Transparent, RoundedCornerShape(12.dp))
-                .width(300.dp),
-            colors = ButtonDefaults.outlinedButtonColors(
-                containerColor = amarillo,
-                contentColor = MaterialTheme.colorScheme.onPrimary
-            ),
-            shape = RoundedCornerShape(12.dp),
-            ) {
+            Spacer(modifier = Modifier.width(8.dp))
             Text(
-                text = "Iniciar sesión",
+                text = text,
+                fontSize = 18.sp,
                 fontWeight = FontWeight.Bold,
-                fontSize = 20.sp
+                color = Color.White
             )
         }
     }
-
 }
 
 @Preview(showBackground = true)
