cc13774630f1eda3517477ef17ede0f695db9244
KIHM-02 <monteroisaac33@gmail.com>
2025-04-07 01:13:04 -0600

    Added the function of pick Images without androids permission

--- app/src/main/java/com/hesdi/silvercare/Views/Medicamento.kt
+++ app/src/main/java/com/hesdi/silvercare/Views/Medicamento.kt
@@ -1,16 +1,30 @@
 package com.hesdi.silvercare.Views
 
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -26,10 +40,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import com.hesdi.silvercare.R
 import com.hesdi.silvercare.ui.theme.SilverCareTheme
 import com.hesdi.silvercare.ui.theme.amarillo
 import com.hesdi.silvercare.ui.theme.azulCielo
@@ -69,7 +88,7 @@ fun MedicamentoView()
             horizontalAlignment = Alignment.CenterHorizontally)
         {
 
-            SpacesBetween(100)
+            SpaceTopBottom(100)
 
             Text(
                 text = "Registro de Medicina",
@@ -81,17 +100,92 @@ fun MedicamentoView()
                     .align(Alignment.CenterHorizontally)
             )
 
-            SpacesBetween(40)
+            SpaceTopBottom(40)
 
             OutlinedInputs("Medicamento","Registrar medicamento")
 
-            SpacesBetween(40)
+            SpaceTopBottom(40)
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                TextosSimples("Subir foto", Color.White)
+                SpaceBetween(20)
+                ImagePicker()
+            }
+
+            SpaceTopBottom(40)
 
             OutlinedInputs("Fecha de caducidad","Registrar fecha de caducidad")
 
-            
         }
     }
+}
+
+@Composable
+fun ImagePicker()
+{
+    /*
+    * Fuente:
+    * https://proandroiddev.com/implementing-photo-picker-on-android-kotlin-jetpack-compose-326e33e83b85
+    * */
+
+    //Almacenamos la foto obtenida por el usuario en una variable
+    var photoUri: Uri? by remember { mutableStateOf(null) }
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia())
+    { uri ->
+        //Cuando el usuario selecciona una imagen, se ejecuta esta función
+        photoUri = uri
+    }
+
+    Column(verticalArrangement = Arrangement.Center) {
+        Button(
+            onClick = {
+                //Cuando presionen el boton, se ejecuta la funcion picker
+                launcher.launch(
+                    PickVisualMediaRequest(
+                        //Aqui solo solicitamos fotos, pueden ser tambien videos pero ocupas
+                        //cambiar a VideoOnly o ImageAndVideo
+                        mediaType = ActivityResultContracts.PickVisualMedia.ImageOnly
+                    )
+                )
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = amarillo,
+                contentColor = Color.Black
+            )
+        ) {
+            Row {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_image_24),
+                    contentDescription = "Ícono de imagen"
+                )
+
+                SpaceBetween(20)
+
+                Text("Imagen")
+            }
+        }
+
+        SpaceTopBottom(5)
+
+        if (photoUri != null) {
+            //Usamos Coil para mostrar la foto
+            val painter = rememberAsyncImagePainter(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(photoUri)
+                    .build()
+            )
+
+            Image(
+                painter = painter,
+                contentDescription = null,
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(200.dp)
+            )
+
+        }
+    }
+
 }
 
 @Composable
@@ -107,7 +201,13 @@ fun TextosSimples(text: String, color: Color)
 }
 
 @Composable
-fun SpacesBetween(dimension: Int)
+fun SpaceBetween(dimension: Int)
+{
+    Spacer(modifier = Modifier.width(dimension.dp))
+}
+
+@Composable
+fun SpaceTopBottom(dimension: Int)
 {
     Spacer(modifier = Modifier.height(dimension.dp))
 }
