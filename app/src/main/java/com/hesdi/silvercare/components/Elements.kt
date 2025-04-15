18cbf32208c9cb66651de5dc633a1f44e0644022
Petterson04 <ferpetterson@gmail.com>
2025-04-15 07:03:52 -0600

    se crea la vista de AgendarCita.kt falta crear la funcionalidad de los botones y crear el controlador

--- app/src/main/java/com/hesdi/silvercare/components/Elements.kt
+++ app/src/main/java/com/hesdi/silvercare/components/Elements.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -36,7 +37,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
@@ -55,6 +58,18 @@ fun TextosSimples(text: String, color: Color)
         style = MaterialTheme.typography.bodyLarge,
         fontWeight = FontWeight.Bold,
         fontSize = 20.sp
+    )
+}
+
+@Composable
+fun Titulo(text: String) {
+    Text(
+        text = text,
+        fontSize = 50.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color.White,
+        textAlign = TextAlign.Center,
+        fontFamily = FontFamily.Cursive
     )
 }
 
