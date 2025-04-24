6b647a5794ee45eaca0a376a4776841387931733
Petterson04 <ferpetterson@gmail.com>
2025-04-24 12:14:31 -0600

    Create CRUD for citas and add funcionalities for the button registrar for create a new cita in the databe and create the funcion for add cita in the google calendary

---
+++ app/src/main/java/com/hesdi/silvercare/Model/Citaviewmodel.kt
@@ -0,0 +1,69 @@
+package com.hesdi.silvercare.Model
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import com.hesdi.silvercare.entities.Cita
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import java.util.UUID
+
+class Citaviewmodel: ViewModel() {
+    private val db= Firebase.firestore
+
+    private var Lista_citas= MutableStateFlow<List<Cita>>(emptyList())
+    val listaCitas = Lista_citas.asStateFlow()
+
+    init {
+        obtenerCitas()
+    }
+
+    fun obtenerCitas() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val result = db.collection("agenda").get().await()
+                val citas = result.documents.mapNotNull {
+                    it.toObject(Cita::class.java)
+                }
+                Lista_citas.value = citas
+            } catch (e: Exception) {
+                e.printStackTrace() // útil para ver el error en Logcat
+            }
+        }
+    }
+
+    fun agregarCita(cita: Cita){
+        cita.id= UUID.randomUUID().toString()
+        viewModelScope.launch(Dispatchers.IO) {
+            db.collection("agenda").document(cita.id).set(cita)
+                .addOnSuccessListener {
+                    obtenerCitas()
+                }
+        }
+    }
+
+    fun actualizarCita(cita: Cita){
+        viewModelScope.launch(Dispatchers.IO) {
+            db.collection("agenda").document(cita.id).update(cita.toMap())
+                .addOnSuccessListener {
+                    obtenerCitas()
+                }
+        }
+    }
+
+    fun borrarCita(id: String){
+        viewModelScope.launch(Dispatchers.IO) {
+            db.collection("agenda").document(id).delete()
+                .addOnSuccessListener {
+                    Lista_citas.value=listaCitas.value.filter {
+                        it.id != id
+                    }
+                }
+        }
+
+    }
+}
